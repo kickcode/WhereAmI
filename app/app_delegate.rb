2fc8ee0ada3221aea80f057eee577e219e27e74d
Elliott Draper <el@kickcode.com>
2016-06-25 23:40:48 +0100

    using CLLocationManager to request user location updates

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -2,6 +2,13 @@ class AppDelegate
   def applicationDidFinishLaunching(notification)
     buildMenu
     buildWindow
+
+    if CLLocationManager.locationServicesEnabled
+      @location_manager = CLLocationManager.alloc.init
+      @location_manager.delegate = self
+      @location_manager.desiredAccuracy = KCLLocationAccuracyHundredMeters
+      @location_manager.startUpdatingLocation
+    end
   end
 
   def buildWindow
@@ -11,5 +18,18 @@ class AppDelegate
       defer: false)
     @mainWindow.title = NSBundle.mainBundle.infoDictionary['CFBundleName']
     @mainWindow.orderFrontRegardless
+  end
+
+  def locationManager(manager, didFailWithError: error)
+    NSLog "[LOCATION] error: #{error.localizedDescription}"
+  end
+
+  def locationManager(manager, didChangeAuthorizationStatus: status)
+    NSLog "[LOCATION] status: #{status}, authorized: #{status == KCLAuthorizationStatusAuthorized}"
+  end
+
+  def locationManager(manager, didUpdateLocations: locations)
+    @current_location = locations.first
+    NSLog "CURRENT LOCATION: #{@current_location.coordinate.latitude},#{@current_location.coordinate.longitude}"
   end
 end
